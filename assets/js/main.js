4dc422f93f2f0b3074cd2ac607ee126be523bdfe
Vasyl Tkachenko <vasyl.tkachenko-pi2301@kep.nung.edu.ua>
2026-06-01 23:43:00 +0300

    Add first creen of template

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,1 +1,21 @@
-console.log("Project started");
+const hamburger = document.getElementById('iconBlock');
+const closeBtn  = document.getElementById('closeMenu');
+const menu      = document.getElementById('mainMenu');
+ 
+hamburger.addEventListener('click', () => {
+  menu.classList.add('menu-open');
+  document.body.style.overflow = 'hidden';
+});
+ 
+closeBtn.addEventListener('click', () => {
+  menu.classList.remove('menu-open');
+  document.body.style.overflow = '';
+});
+ 
+menu.querySelectorAll('a').forEach(link => {
+  link.addEventListener('click', () => {
+    menu.classList.remove('menu-open');
+    document.body.style.overflow = '';
+  });
+});
+ 
